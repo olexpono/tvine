b25c3afcd1ae3c4468d6a86ebca1446048d3fe39
Olex Ponomarenko <tholex@gmail.com>
2013-01-31 17:14:41 -0500

    sweet googley moogley

--- public/js/templates.js
+++ public/js/templates.js
@@ -1,1 +1,1 @@
-var TMPL = {"test":"Hello {{bob}}! ","tag":"<li>   <span class=\"tag\" rel=\"tag\" data-hashtag=\"{{tag}}\" data-count=\"{{count}}\" href=\"javascript:void(0);\">{{tag}}<a class=\"close\" href=\"javascript:void(0);\">&times;</a>   </span> </li> "};
+var TMPL = {"test":"Hello {{bob}}! ","tag":"<li>   <span class=\"tag\" rel=\"tag\" data-hashtag=\"{{tag}}\" data-count=\"{{count}}\" href=\"javascript:void(0);\"><span>{{tag}}</span><a class=\"close\" href=\"javascript:void(0);\">&times;</a>   </span> </li> "};
