9a87e9e183353570f33c3e99a57252bae48dd076
Olex Ponomarenko <tholex@gmail.com>
2013-01-30 19:52:24 -0500

    css pasted, main.js started with tags route

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,29 @@
-$(function() {
+$.TVine = {
+  init: function() {
+    this.setupRoutes();
+  },
+  setupRoutes: function() {
+    crossroads.addRoute(
+      "/{tagstring}",
+      function(tagstring) {
+        console.log("navigated to tags" + tagstring);
+        // From here, split by +, fetch videos, and play
+      }
+    );
+  }
+}
 
+
+$(function() {
   console.log("templates", TMPL);
 
-  $("#output").html(
-    Mustache.render(TMPL.test, DATA)
-  );
+  $.TVine.init();
+  crossroads.routed.add(console.log, console);
+  var parseHash = function(newHash, oldHash) {
+    crossroads.parse(newHash);
+  }
 
+  hasher.initialized.add(parseHash);
+  hasher.changed.add(parseHash);
+  hasher.init();
 });
