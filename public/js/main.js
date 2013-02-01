af3a7b185fd73c5003c4394e19a4d2884fb0a5e8
Olex Ponomarenko <tholex@gmail.com>
2013-02-01 01:32:06 -0500

    Switch to to using underscore hacks for playlist management.

--- public/js/main.js
+++ public/js/main.js
@@ -5,10 +5,10 @@ need to store as an object
 
 $.TVine = {
   init: function() {
+    this.tagData = {};
     this.currentTags  = [];
     this.previousTags = [];
     this.playlist     = [];
-    this.tagDataList  = [];
     this.totalVideos = 0;
     this.currentIdx  = 0;
     this.videoRef    = {};
@@ -25,7 +25,7 @@ $.TVine = {
     crossroads.addRoute(
       "/{tagstring}",
       function(tagstring) {
-        console.log("navigated to tags" + tagstring);
+        console.log("Navigated to tags" + tagstring);
         // From here, split by +, fetch videos, and play
         console.log("Split ", tagstring.split("+"));
 
@@ -61,39 +61,39 @@ $.TVine = {
    * Updates previousTags to currentTags */
   refreshFeed: function() {
     this.totalVideos=0;
-    console.log("Refreshing feed.");
     var newTags =
       _.filter(this.currentTags,
                function(tag) { return $.TVine.previousTags.indexOf(tag) < 0; });
-    console.log("new tags: " + newTags);
 
     var removedTags =
       _.filter(this.previousTags,
                function(tag) { return $.TVine.currentTags.indexOf(tag) < 0; });
-    console.log("removed tags: " + removedTags);
     var end_of_array_check = newTags.length;
     var count=0;
     _.each(newTags,
       function(tag) {
         $.get('/query/' + tag, function(data) {
           $.TVine.addTag(tag, data);
-          // re-interleave the videos each time you add a new tag.
-          $.TVine.interleaveVideos();
         });
       }
     );
     _.each(removedTags,
       function(tag) {
         $.TVine.removeTag(tag);
-        $.TVine.interleaveVideos();
       }
     );
   },
+
   /* Utility used by refreshFeed, careful using this directly */
   addTag: function(tag, data) {
     this.previousTags.push(tag);
-    this.receiveVideos(data);
     this.renderNewTag(tag, data.data.count);
+
+    if (data.data.records.length > 1){
+      this.addVideos(tag, data.data.records);
+    } else {
+      /* TODO -- no results found */
+    }
   },
   /* Utility used by refreshFeed, careful using this directly */
   removeTag: function(tag) {
@@ -101,21 +101,23 @@ $.TVine = {
       _.filter(this.previousTags,
                function(prevtag) { return prevtag != tag; });
     $(".tags [data-hashtag='" + tag + "']").parent().remove();
-    /* TODO -- update video pool on hashtag deletion */
+
+    this.removeVideos(tag);
   },
 
   /* circular list */
   getNextVideo: function(){
-    this.currentIdx = (this.currentIdx+1) % this.playlist.length;
-    return this.playlist[this.currentIdx];
-    //load into view
+    /* TODO - fetch more than one page for given tags */
+    /* Idea for paging: If playlist.shift() is the last video in any tagData, fetch more for that tag */
+    this.playlist.push(this.playlist.shift());
+    return this.playlist[0];
   },
 
   /* circular list */
   getPreviousVideo: function(){
-    this.currentIdx = Math.abs((this.currentIdx-1) % this.playlist.length);
-    return this.playlist[this.currentIdx];
-    //load into view
+    this.playlist.unshift(_.last(this.playlist));
+    this.playlist.pop();
+    return this.playlist[0];
   },
 
   loadNextVideo: function(){
@@ -123,24 +125,58 @@ $.TVine = {
     this.video_ref.play();
   },
 
+  addVideos: function(tag, records) {
+    var spacing = 1;
+    if (typeof this.tagData[tag] == "undefined") {
+      this.tagData[tag] = records;
+    }
+    /* Inject empty values into records to space them out,
+     * then zip them with the current playlist.
+     * more active tags => more empty values between each of the new videos */
+    spacing = Math.floor(_.size(this.tagData) / 2);
+    /* [4] */
+    records = _.reduce(
+      records,
+      function (paddedArray, record) {
+        paddedArray.push(record);
+        for (var i = 0; i < spacing; i++) {
+          paddedArray.push(undefined);
+        }
+        return paddedArray;
+      },
+      []
+    );
+
+    this.playlist =
+      _.compact(
+        _.flatten(
+          _.zip(this.playlist, records)
+        )
+      );
+  },
+
+  removeVideos: function(tag) {
+    if (typeof this.tagData[tag] == "undefined") {
+      return;
+    }
+    this.playlist = _.filter(
+      this.playlist,
+      function(queued) {
+        return $.TVine.tagData[tag].indexOf(queued) < 0;
+      }
+    );
+  },
+
   interleaveVideos: function(){
-    var that = this;
     for(var i=0;i < this.totalVideos; i++){
 
-      var current = this.tagDataList[ i % this.tagDataList.length ];
+      var current = this.tagData[ i % this.tagDataList.length ];
       if(current){
         this.playlist.push(current.shift())
         if(current.length == 0){
           this.tagDataList.splice( i % this.tagDataList.length,1);
         }
       }
-    }
-  },
-
-  receiveVideos: function(data) {
-    this.totalVideos += data.data.records.length;
-    if (data.data.records.length > 1){
-      this.tagDataList.push(data.data.records);
     }
   },
 
